33b70090214b0a9d75b53c6ee07270c5fbce8ed7
samuelSavanovic <samuel.savanovic@gmail.com>
2017-05-12 12:51:20 +0200

    minor improvment
    Signed-off-by: samuelSavanovic <samuel.savanovic@gmail.com>

--- corpora_builder.py
+++ corpora_builder.py
@@ -1,13 +1,14 @@
 from goodreads_books import get_links
 from scrapper import parse_comments
 
-for l in get_links(10):
-    comments, dates = parse_comments(l)
-    i = 0
-    book_name = l.split('/')[-1].split('.')[-1]
-    for comment in comments:
-        print(comment)
-        is_pos = input("pos/neg?")
-        with open("corpora/" + is_pos + "_" + book_name + "_" + dates[i] + "_" + str(i) + ".txt", "w") as f:
-            f.write(comment)
-        i += 1
+if __name__ == "__main__":
+    for l in get_links(10):
+        comments, dates = parse_comments(l)
+        i = 0
+        book_name = l.split('/')[-1].split('.')[-1]
+        for comment in comments:
+            print(comment)
+            is_pos = input("pos/neg?")
+            with open("corpora/" + is_pos + "_" + book_name + "_" + dates[i] + "_" + str(i) + ".txt", "w") as f:
+                f.write(comment)
+            i += 1
